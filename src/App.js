23bc0f1f21d8de32d1e376518f6a42f34bfd4b4c
felipemacielsantos <felipe.maciel@mail.com>
2021-09-26 10:47:08 -0300

    last-commit
    last one

--- src/App.js
+++ src/App.js
@@ -17,10 +17,12 @@ export default class App extends Component{
     }
     
     addDigit = n => {
-        if (n === '.' && this.state.displayValue.includes('.')){
+        console.debug(typeof this.state.displayValue)
+        
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')){
             return 
         }
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
 
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
@@ -41,7 +43,25 @@ export default class App extends Component{
     }
 
     setOperation = operation => {
-
+        if (this.state.current === 0){
+            this.setState({operation, current: 1, clearDisplay: true})
+        }else{
+            const equals = operation === '='
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+            }catch (e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+            this.setState({
+                displayValue: `${values[0]}`,
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values,
+            })
+        }
     }
     render() {
         return(
